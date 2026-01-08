4dd5f301b349130d007a612564b8f0d26b0f5fbf
Santiago Orejuela <sorejuela18@gmail.com>
2026-01-08 15:25:51 -0500

    Add access capabilities for selfdiscoverytracker module

---
+++ db/access.php
@@ -0,0 +1,25 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+    'mod/selfdiscoverytracker:addinstance' => array(
+        'riskbitmask' => RISK_XSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    ),
+    'mod/selfdiscoverytracker:view' => array(
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => array(
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        )
+    ),
+);
